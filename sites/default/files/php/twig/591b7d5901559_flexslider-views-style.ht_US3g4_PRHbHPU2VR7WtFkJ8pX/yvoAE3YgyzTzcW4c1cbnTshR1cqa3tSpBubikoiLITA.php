f72c0b2e2ed27a75cae22e8ecdf555cdefb460f1
Roald Umandal <umandalroald@gmail.com>
2017-05-17 07:32:03 +0800

    Initial commit

---
+++ sites/default/files/php/twig/591b7d5901559_flexslider-views-style.ht_US3g4_PRHbHPU2VR7WtFkJ8pX/yvoAE3YgyzTzcW4c1cbnTshR1cqa3tSpBubikoiLITA.php
@@ -0,0 +1,91 @@
+<?php
+
+/* modules/contrib/flexslider/flexslider_views/templates/flexslider-views-style.html.twig */
+class __TwigTemplate_b5ab5505a1cf1155869cd34e0171171cff5ea4aa160615f63816380e1c24b0ec extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 13
+        echo "
+<div";
+        // line 14
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["attributes"]) ? $context["attributes"] : null), "html", null, true));
+        echo ">
+    ";
+        // line 15
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["content"]) ? $context["content"] : null), "html", null, true));
+        echo "
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/flexslider/flexslider_views/templates/flexslider-views-style.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  50 => 15,  46 => 14,  43 => 13,);
+    }
+
+    public function getSource()
+    {
+        return "{#
+/**
+ * @file
+ * Default output for a Views style FlexSlider object.
+ *
+ * Available variables:
+ * - attributes: An array of attributes to apply to the element.
+ * - content: A renderable array of the flexslider object
+ *
+ * @see template_preprocess_flexslider_views_style()
+ */
+#}
+
+<div{{ attributes }}>
+    {{ content }}
+</div>
+";
+    }
+}
